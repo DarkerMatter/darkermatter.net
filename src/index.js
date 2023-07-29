89d9aa6a43e3f391337353ca3ca35501647c8df7
DarkerMatter <darker@darker.systems>
2023-07-28 20:42:24 -0700

    Somedays I hate react because of this file....

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import App from './App';
 
+// ReactDOM.render( // This is the old way of rendering React components to the DOM
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
