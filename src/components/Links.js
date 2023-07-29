e98336b9cce0a588406a7fd4965012df739ab38e
DarkerMatter <darker@darker.systems>
2023-07-28 20:42:46 -0700

    Css....

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styles from '../css/App.module.css';
 
+// Links is a component that renders the about links.
+// I Lowkey don't like this, but it's the only way to do it without a bunch of CSS
 function Links() {
     const links = [
         { name: 'Discord', url: 'https://discordapp.com/users/darkermatter' },
@@ -8,7 +10,7 @@ function Links() {
         { name: 'Github', url: 'https://github.com/DarkerMatter' },
         { name: 'Hosting Project', url: 'https://darker.systems' },
     ];
-
+    // More demonstration of my hate of CSS
     return (
         <div className={styles.app}>
             <div className={styles.linksContainer}>
